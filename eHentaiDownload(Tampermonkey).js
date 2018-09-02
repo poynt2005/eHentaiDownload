372385fe4d9df0d5d51d3749feff29f45e89d2d8
紀克勤 <poynt2010@hotmail.com>
2018-09-02 21:38:48 +0800

    Update eHentaiDownload(Tampermonkey).js

--- eHentaiDownload(Tampermonkey).js
+++ eHentaiDownload(Tampermonkey).js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         下載eHentai圖片!!
-// @namespace    eHentai
-// @version      0.1
+// @namespace    https://gist.github.com/poynt2005
 // @description  Get the eHentai Gallery download link
 // @author       紀克勤
 // @match        *://*.e-hentai.org/g/*
@@ -15,139 +14,154 @@
     'use strict';
 
     /*
-		Script that helps user download ehentai/exhentai gallery pictures
-		Require Software: Internet Download Manager software
-	*/
+			Script that helps user download ehentai/exhentai gallery pictures
+			Require Software: Internet Download Manager software
+		*/
 
+    function postParsingRstPage(inputText){
 
-    //inject jQuery (use Tampermonkey require)
+        inputText = inputText.replace(new RegExp("src" , "gm") , "dataSrc");
 
-    //get pics real links and append to page
-    function appendLinksPage(){
-        //result block
-        var picResultBlock = document.createElement("div");
-        var ResultBlockName = document.createElement("span");
-        var PicResultDiv = document.createElement("div");
-        var newPicResDivID = "ResultDIV";
-        PicResultDiv.setAttribute("id" , "newPicResDivID"); //要插入的
-        ResultBlockName.innerHTML = "<h1>圖片下載連結(搭配IDM下載)</h1>";
-        var tipP = document.createElement("p");
-        tipP.innerHTML = "**** <b>複製以下所有連結並複製，開啟IDM -> 任務 -> 從剪貼簿新增批次下載，以加入連結<b> **** <br><br>";
-
-        picResultBlock.appendChild(ResultBlockName);
-        picResultBlock.appendChild(tipP);
-        picResultBlock.appendChild(PicResultDiv);
-        document.getElementsByTagName('body')[0].appendChild(picResultBlock);
-
-
-        //get real pic link
-        function getPICHTML(inputHref , divName){
-            $.ajax({
-                url : inputHref,
-                type: 'GET',
-                success : function(data){
-                    var tmpDiv = document.createElement("div");
-                    tmpDiv.innerHTML = data;
-
-                    var tmpI3 = $($(tmpDiv).find("#i3")[0]);
-                    var tmpI7 = $($(tmpDiv).find("#i7")[0]);
-                    tmpI3.removeAttr("id");
-                    tmpI7.removeAttr("id");
-                    tmpI3.attr("class" , "i3");
-                    tmpI7.attr("class" , "i7");
-                    var resImgSrc = "";
-                    resImgSrc = $(tmpI3.find("img")[0]).attr("src");
-                    var resultSpan = document.createElement("span");
-                    resultSpan.setAttribute("class" , "imgLinks");
-                    if(tmpI7.text()){
-                        resImgSrc = $(tmpI7.find("a")[0]).attr("href");
-                    }
-                    else{
-                        var picName = resImgSrc.split("/").slice(-1)[0];
-                        resultSpan.setAttribute("picName" , picName);
-                    }
-                    resultSpan.innerHTML = resImgSrc + "<br>";
-                    document.getElementById(divName).appendChild(resultSpan);
-                },
-				
-				//if error call the server again
-               error : function(err){
-                   getPICHTML(inputHref , divName);
-               }
-            });
-        }
-
-        for(let i of $(".pic-Page-URL-Span"))
-            getPICHTML($(i).text() , "newPicResDivID");
+        var noScript = document.createElement("noscript");
+        var div = document.createElement("div");
+        noScript.appendChild(div);
+        div.innerHTML = inputText;
+        return noScript;
     }
 
-    //get all pages of gallery
-    function appendPage(){
-        var pageArr = new Array();
-        for (let i of $($(".ptt")[0]).find('a'))
-            if($(i).text()!='>')
-                pageArr.push($(i).attr("href"));
 
-        function getHTML(inputHref , newDivID){
-            $.ajax({
-                url : inputHref,
-                type: 'GET',
-                success : function(data){
-                    document.getElementById(newDivID).innerHTML = "<h3>" + newDivID + "</h3>" + "<br>";
-                    var tmpDiv = document.createElement("div");
-                    tmpDiv.innerHTML = data;
-                    var tmpGdt = $($(tmpDiv).find("#gdt")[0]);
-                    for(let i of tmpGdt.find("a")){
-                        var picURL = $(i).attr("href");
 
-                        var picPageURLSpan = document.createElement("span");
-                        picPageURLSpan.setAttribute("class" , "pic-Page-URL-Span");
-                        picPageURLSpan.innerHTML = picURL + "<br>";
-                        document.getElementById(newDivID).appendChild(picPageURLSpan);
-                    }
-                },
-				// if error call server again
-				error : function(err){
-					getHTML(inputHref , newDivID);
-				}
-            });
-        };
+    async function getAllPicURL(){
 
-        for(let i = 0 ; i < pageArr.length ; i++){
-            var newDiv = document.createElement("div");
-            var newDivID = "result" + i;
-            newDiv.setAttribute("id" , newDivID);
-            newDiv.setAttribute("style" , "display:none");
-            document.getElementsByTagName('body')[0].appendChild(newDiv);
-            getHTML(pageArr[i] , newDivID);
+        var totalPage = ((pageDiv) => {
+            let MAX = -1;
+            for(let i of pageDiv)
+                if(parseInt(i.innerText) > MAX)
+                    MAX = parseInt(i.innerText);
+            return MAX;
+        })($($($(".ptt")[0]).find("td")).toArray());
+
+        function findPicURL(inputDiv){
+            var gdtms = $($($(inputDiv).find("#gdt")[0]).find(".gdtm")).toArray();
+            var rstArr = [];
+            for(let gdtm of gdtms)
+                rstArr.push($(gdtm).find("a")[0].href);
+            return rstArr;
         }
-    };
 
-    var mybtnDiv = document.createElement("div");
-    mybtnDiv.setAttribute("style" , "text-align:center;position:relative;");
+        function getAllPage(inputUrl){
+            return new Promise(function(resolve, reject){
+                $.ajax({
+                    type: "GET",
+                    url: inputUrl,
+                    success: function(data){
+                        var rstDiv = postParsingRstPage(data);
+                        resolve(findPicURL(rstDiv));
+                    }
+                });
+            });
+        }
 
-    var myButton = document.createElement("a");
-    myButton.setAttribute("style" , "padding:5px 15px;color:white;border-radius:5px;background-color:#9999ff;");
-    myButton.setAttribute("id" , "start-download");
-    myButton.href = "javascript:void(0)";
-    myButton.innerHTML = "下載圖集";
-
-    var tipSpan = document.createElement("span");
-    tipSpan.setAttribute("style" , "color:#00bfff;");
-    tipSpan.innerHTML = "<h3>請搭配Internet Download Manager下載底下出現的連結</h3><br>";
-
-    mybtnDiv.appendChild(myButton);
-    mybtnDiv.appendChild(tipSpan);
-
-    document.getElementById("taglist").append(mybtnDiv);
-
-    $("#start-download").click(function(){
-        var myWaitingTime = prompt("請輸入抓取目前分頁估算時間(秒)");
-        if(myWaitingTime)
-            if(!isNaN(myWaitingTime)){
-                appendPage();
-                setTimeout(appendLinksPage , myWaitingTime * 1000);
+        async function recurGetAllPage(idx , rstArr){
+            if(idx == totalPage)
+                return rstArr;
+            else{
+                var pageURL = `${window.location.href}?p=${idx}`;
+                rstArr = rstArr.concat(await getAllPage(pageURL));
+                return recurGetAllPage(idx+1 , rstArr)
             }
+        }
 
-    });
+        var currentPagePicArr = findPicURL(postParsingRstPage($("html").html()));
+        currentPagePicArr = currentPagePicArr.concat(await recurGetAllPage(1 , []));
+        return currentPagePicArr;
+    }
+
+
+
+    async function getAllPicDownloadLink(picURLs){
+        function getPicRealLink(inputURL){
+            return new Promise(function(resolve , reject){
+                $.ajax({
+                    type: "GET",
+                    url: inputURL,
+                    success: function(data){
+                        var rstDiv = postParsingRstPage(data);
+                        var picSrc = $($(rstDiv).find("#i3")[0]).find("img")[0].getAttribute("datasrc");
+                        var originalSrc = ((rstObj) => {
+                            if(rstObj.length == 0)
+                                return false;
+                            else
+                                return
+                            rstObj[0].href;
+                        })($($(rstDiv).find("#i7")[0]).find("a"));
+                        resolve((originalSrc) ? originalSrc : picSrc);
+                    }
+                });
+            });
+        }
+
+        async function recurGetPicRealLink(urlArr , idx , rstArr){
+            if(idx == urlArr.length)
+                return rstArr;
+            else{
+                rstArr = rstArr.concat(await getPicRealLink(urlArr[idx]));
+                return recurGetPicRealLink(urlArr , idx+1 , rstArr);
+            }
+        }
+
+        var rstArr = [];
+        rstArr = rstArr.concat(await recurGetPicRealLink(picURLs , 0 , []));
+
+        return rstArr;
+    }
+
+
+    function appendPage(linkArr){
+        var rstBlock = document.createElement("div");
+        var h2P = document.createElement("p");
+        var h2 = document.createElement("h2");
+        h2.innerHTML = "複製以下所有連結並複製，開啟IDM -> 任務 -> 從剪貼簿新增批次下載，以加入連結";
+
+        var totalPageHint = document.createElement("p");
+        totalPageHint.innerHTML = `總共找到${linkArr.length}個鏈結`;
+
+        h2P.appendChild(h2);
+        rstBlock.appendChild(h2P);
+        rstBlock.appendChild(totalPageHint);
+
+        for(let i of linkArr){
+            let p = document.createElement("p");
+            p.innerHTML = i;
+            rstBlock.appendChild(p);
+        }
+
+        document.body.appendChild(rstBlock);
+    }
+
+
+    (function(){
+        var mybtnDiv = document.createElement("div");
+        mybtnDiv.setAttribute("style" , "text-align:center;position:relative;");
+
+        var myButton = document.createElement("a");
+        myButton.setAttribute("style" , "padding:5px 15px;color:white;border-radius:5px;background-color:#9999ff;");
+        myButton.onclick = async function(){
+            var picURLs = await getAllPicURL();
+            var picDownLink = await getAllPicDownloadLink(picURLs);
+            appendPage(picDownLink);
+
+        }
+        myButton.href = "javascript:void(0)";
+        myButton.innerHTML = "下載圖集";
+
+        var tipSpan = document.createElement("span");
+        tipSpan.setAttribute("style" , "color:#00bfff;");
+        tipSpan.innerHTML = "<h3>請搭配Internet Download Manager下載底下出現的連結</h3><br>";
+
+        mybtnDiv.appendChild(myButton);
+        mybtnDiv.appendChild(tipSpan);
+
+        document.getElementById("taglist").append(mybtnDiv);
+    })();
 })();
